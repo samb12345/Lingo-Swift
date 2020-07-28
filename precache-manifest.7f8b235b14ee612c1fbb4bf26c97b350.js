fba2be06ef79c30222f99dadd67ba185cfb5421d
Sam Buchan <bsam@live.co.uk>
2020-07-28 07:41:10 +0100

    Updates

--- precache-manifest.7f8b235b14ee612c1fbb4bf26c97b350.js
+++ precache-manifest.7f8b235b14ee612c1fbb4bf26c97b350.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ab10e930781d647517763b2437523a50",
+    "revision": "dbe8820a2bdaecae56bad0ebf91d14c1",
     "url": "/Lingo-Swift/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Lingo-Swift/static/css/2.f65acde2.chunk.css"
   },
   {
-    "revision": "94c92808ca62eacb0c39",
+    "revision": "711ec3ab5186f015037e",
     "url": "/Lingo-Swift/static/css/main.62f0957c.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Lingo-Swift/static/js/2.8831c7d9.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "94c92808ca62eacb0c39",
-    "url": "/Lingo-Swift/static/js/main.e581ce09.chunk.js"
+    "revision": "711ec3ab5186f015037e",
+    "url": "/Lingo-Swift/static/js/main.d638f157.chunk.js"
   },
   {
     "revision": "a7c4c093fdda0b74bbf9",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Lingo-Swift/precache-manifest.e194fda0628db60328a87f8c902ecc34.js"
+  "/Lingo-Swift/precache-manifest.7f8b235b14ee612c1fbb4bf26c97b350.js"
 );
 
 self.addEventListener('message', (event) => {
