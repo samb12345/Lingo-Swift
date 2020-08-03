a44ede27fde8c4012062d816d2888ddadaedba3a
Sam Buchan <bsam@live.co.uk>
2020-08-03 09:14:22 +0100

    Updates

---
+++ precache-manifest.e4cedfe7e9e855f3aaf96b88fb89af26.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d5e472f78acf4bdad23fa10dbe3669d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9bce68fc98907b4bb6ed",
+    "url": "/static/css/2.f65acde2.chunk.css"
+  },
+  {
+    "revision": "ff860c91f771795816e0",
+    "url": "/static/css/main.09df5e0f.chunk.css"
+  },
+  {
+    "revision": "9bce68fc98907b4bb6ed",
+    "url": "/static/js/2.c42058f6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c42058f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff860c91f771795816e0",
+    "url": "/static/js/main.1fdd2145.chunk.js"
+  },
+  {
+    "revision": "96a9ba297977d9f3a8b9",
+    "url": "/static/js/runtime-main.5b21e427.js"
+  }
+]);
